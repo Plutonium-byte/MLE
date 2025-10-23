f9ab5c7e6dd68ca3cf3154c4ae495c17c4f24fb2
Plutonium-byte <shaunsamuel057@gmail.com>
2025-10-23 11:22:22 +0530

    Create Q1-2.py

---
+++ 05-deployment/Q1-2.py
@@ -0,0 +1,5 @@
+## Question 1 - What's the version of uv you installed?
+print("uv 0.9.5")
+
+## Question 2 - What's the first hash for Scikit-Learn you get in the lock file?
+print("sha256:b4fc2525eca2c69a59260f583c56a7557c6ccdf8deafdba6e060f94c1c59738e")
